87dd5e229a71fceab3ce52a54a6c5c27f209c2bf
Alleksey5 <alexeyvar2005@gmail.com>
2025-02-10 13:53:47 +0300

    new commits

--- src/utils/init_utils.py
+++ src/utils/init_utils.py
@@ -160,3 +160,12 @@ def setup_saving_and_logging(config):
     logger.setLevel(logging.DEBUG)
 
     return logger
+
+def init_weights(m, mean=0.0, std=0.01):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        m.weight.data.normal_(mean, std)
+
+def get_padding(kernel_size, dilation=1):
+    return int((kernel_size*dilation - dilation)/2)
+
